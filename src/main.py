b9cbd1449272c7199f55b92a3e5d61c5af15f5c6
Sampsa Ollikainen <sampsa.ollikainen@gmail.com>
2022-11-29 19:04:30 +0200

    luotu testiluokka, lisätty invoke komennot ja luotu changelog

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from ui import UserInterface
+from command_io import ConsoleIO
 
-start_generator = UserInterface()
+start_generator = UserInterface(ConsoleIO())
 start_generator.start()
--- src/ui.py
+++ src/ui.py
@@ -1,21 +1,22 @@
 from classes import Classes
 from quests import Quests
 from races import Races
-from command_io import ConsoleIO
 ### Code still lacks comments due to shortness of time ###
 
 class UserInterface:
-    def __init__(self):
+    def __init__(self, io):
         self._generated_playthroughs = []
+        self._printed_text = []
         self._guide_text = "Commands:\n0 quit\n1 generate a playthrough\n2 print all generated playthroughs\n"
-        self._io = ConsoleIO()
+        self._io = io
 
     def start(self):
         print(self._guide_text)
         while True:
             key = self._io.read("command: ")
             if key not in ["0", "1", "2"]:
-                print("False input!")
+                self._io.printout("False input!")
+                self._printed_text.append("False input!")
                 print(self._guide_text)
                 continue
             if key == "0":
@@ -26,9 +27,11 @@ class UserInterface:
                 quest = Quests(playstyle).primary_questline()
 
                 playthrough = f"Your race will be {race} and your class will be {playstyle}. Your primary questline is {quest}"
-                print(playthrough)
+                self._io.printout(playthrough)
+                self._printed_text.append(playthrough)
                 self._generated_playthroughs.append(playthrough)
             else:
                 for i in self._generated_playthroughs:
-                    print(i)
+                    self._printed_text.append(i)
+                    self._io.printout(i)
 
